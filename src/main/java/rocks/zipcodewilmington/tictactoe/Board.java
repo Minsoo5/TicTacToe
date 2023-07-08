52a956483689c9eab27752a36f2db2c640091b40
min <minsoojo5@gmail.com>
2023-07-08 11:40:52 -0400

    Win and tie conditions completed

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,7 +4,11 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    
+    Character[][] board;
+    
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
@@ -16,11 +20,51 @@ public class Board {
     }
 
     public Boolean isTie() {
-        return null;
+        if(getWinner() == "");
+        return true;
     }
 
     public String getWinner() {
-        return null;
+        String winner = "";
+        if (checkRowFor('X') == true || checkColumnFor('X') == true || checkDiag('X') == true) {
+            winner = "X";
+        } else if (checkColumnFor('Y') == true || checkColumnFor('Y') == true || checkDiag('Y') == true) {
+            winner = "Y";
+        } else winner = "";
+        return winner;
+    }
+
+
+    public Boolean checkRowFor(char check) {
+        //board[row][col]
+        boolean didWin = false;
+        if     ((board[0][0] == check && board[0][1] == check && board[0][2] == check) ||
+                (board[1][0] == check && board[1][1] == check && board[1][2] == check) ||
+                (board[2][0] == check && board[2][1] == check && board[2][2] == check)) {
+            didWin = true;
+        }
+        return didWin;
+        }
+
+
+    public Boolean checkColumnFor(char check) {
+        //board[row][col]
+        boolean didWin = false;
+        if     ((board[0][0] == check && board[1][0] == check && board[2][0] == check) ||
+                (board[0][1] == check && board[1][1] == check && board[2][1] == check) ||
+                (board[0][2] == check && board[1][2] == check && board[2][2] == check)) {
+            didWin = true;
+        }
+        return didWin;
+    }
+
+    public Boolean checkDiag(char check) {
+        boolean didWin = false;
+        if ((board[0][0] == check && board[1][1] == check && board[2][2] == check)) {
+            didWin = true;
+        }
+
+        return didWin;
     }
 
 }
